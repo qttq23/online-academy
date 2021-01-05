f82986dab602d461ce8da86a86bbed71316cec4b
ducminhcao <minhduc.itus@gmail.com>
2021-01-05 11:26:44 +0700

    add navbar unstyle

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import {Button} from "@material-ui/core";
 import Footer from "./components/Footer";
+import PrimarySearchAppBar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-        <Button color={"primary"} variant={"contained"}>Test</Button>
+        <PrimarySearchAppBar></PrimarySearchAppBar>
         <div>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet architecto consequatur corporis dolorem eaque eius eveniet, expedita iusto mollitia nemo nobis, omnis perferendis praesentium quo reiciendis similique sit voluptatibus.</p>
         </div>
